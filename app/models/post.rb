d99783d0231cee162676c0c4ac387819d7782fcc
Luis Miguel <oluismrs@gmail.com>
2024-04-18 21:27:23 -0300

    Adicionar mensagens personalizadas para validações

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   paginates_per 3
 
-  validates :title, presence: true, length: { maximum: 150 }
-  validates :body, presence: true
+  validates :title, presence: { message: "Title cannot be blank" }, length: { maximum: 150, message: "Title cannot exceed 150 characters" }
+  validates :body, presence: { message: "Title cannot be blank" }
 end

c9e3d52d9363e7cb82ca4e90f81d7c399e0cc33e
Luis Miguel <oluismrs@gmail.com>
2024-04-18 22:16:58 -0300

    Adicionar mensagens personalizadas no modelo Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   paginates_per 3
 
   validates :title, presence: { message: "Title cannot be blank" }, length: { maximum: 150, message: "Title cannot exceed 150 characters" }
-  validates :body, presence: { message: "Title cannot be blank" }
+  validates :body, presence: { message: "Body cannot be blank" }
 end
